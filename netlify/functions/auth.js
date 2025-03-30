d5a0e7faad165e26995938d31c574fbd56f33cdb
vibration-resonancegit config --global user.email <fromthenext77@gmail.com>
2025-03-30 23:49:25 +0200

    oauth update

---
+++ netlify/functions/auth.js
@@ -0,0 +1,34 @@
+// Fonction Netlify pour gérer l'authentification Auth0
+exports.handler = async (event, context) => {
+  const redirectUrl = 'https://clean-eau-nantes.netlify.app/admin/callback';
+  
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html'
+    },
+    body: `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <meta charset="UTF-8">
+        <title>Authentification</title>
+        <script src="https://cdn.auth0.com/js/auth0/9.14/auth0.min.js"></script>
+      </head>
+      <body>
+        <script>
+          const auth0 = new auth0.WebAuth({
+            domain: 'dev-x3odl65g8c07cjf5.us.auth0.com',
+            clientID: 'fHdNXEZgsI166aHZfFu6GcDNtCzZoefC',
+            redirectUri: '${redirectUrl}',
+            responseType: 'token id_token',
+            scope: 'openid profile email'
+          });
+          
+          auth0.authorize();
+        </script>
+      </body>
+      </html>
+    `
+  };
+}; 
